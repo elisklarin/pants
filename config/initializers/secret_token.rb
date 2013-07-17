d5baf59b96879e562cae50c17a5437b8882dbced
Eli Sklarin <eli.sklarin@gmail.com>
2013-07-17 15:28:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pants::Application.config.secret_key_base = '1a06adfa9e422c584fd1c642ae6e57026e0108ede5c34b8f1f2d8ee13d5e5c13d07c019bd409390dc08b0a0557631222d6da7ba0e4bb83ea51fb8c348583dd85'
